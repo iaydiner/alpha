a2c9d9dbedd08d4b868add1d6e6bdb4e8bee76e3
iaydiner <lysydnr@gmail.com>
2016-08-20 03:16:31 -0400

    create new template

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'pages#home'
+  get   'about', to: 'pages#about'
 
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +16,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
 
   # Example resource route with options:
   #   resources :products do
