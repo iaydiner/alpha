924057a51782e90a27f13c83e7d1eabfffae1189
iaydiner <lysydnr@gmail.com>
2016-08-21 12:39:37 -0400

    edit page done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,12 @@ class ArticlesController < ApplicationController
 
 	end
 
+	def edit
+		@article=Article.find(params[:id])
+		
+
+	end
+
 	def create
 		#render plain: params[:article].inspect
 		#@article=Article.new(article_params)
@@ -19,9 +25,21 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article=Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice]="Article was successfully update!"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
 	def show
 		@article=Article.find(params[:id])
 	end
+
+
 
 	private
 	 def article_params
